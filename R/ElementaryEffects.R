4f6bc5922e374195316a5ee9df858573653652bd
Rob Moss <rob.rgm@gmail.com>
2011-03-02 15:38:59 +0100

    ElementaryEffects() can now remove outliers automatically.
    This uses the "median/MAD" method presented in the AMC Technical Brief
    number 6, April 2001. Royal Society of Chemistry, ISSN 1757-5958.

--- R/ElementaryEffects.R
+++ R/ElementaryEffects.R
@@ -3,14 +3,14 @@
 #
 # Args:
 #   experiments: the data frame of delta-perturbation experiments.
-#   na.rm:       whether to remove NA values before calculating statistics.
+#   outliers.rm: whether to remove outliers before calculating statistics.
 #   stats.only:  whether to only return the means and deviations.
 #
 # Returns:
 #   A list of the elementary effects (effects), the mean effects (mean) and
 #   standard deviation of effects (sd) for each parameter in the data frame.
 #
-ElementaryEffects <- function(experiments, na.rm=TRUE, stats.only=TRUE) {
+ElementaryEffects <- function(experiments, outliers.rm=TRUE, stats.only=TRUE) {
     exp.count <- dim(experiments)[1]
     names.all <- names(experiments)
     names.par <- grep("p_", names.all, value=TRUE)
@@ -34,15 +34,48 @@ ElementaryEffects <- function(experiments, na.rm=TRUE, stats.only=TRUE) {
         vars.pre <- experiments[ix.pre[delta.exps], names.var]
         vars.post <- experiments[ix.post[delta.exps], names.var]
         
-        # NOTE: vars.pre can contain zeros.
+        # Ignore any experiments where the parameter was zero prior to the
+        # delta perturbation, as to avoid dividing by zero when normalising.
+        vars.post[vars.pre == 0] <- NA
+        vars.pre[vars.pre == 0] <- NA
+
         vars.diff <- (vars.post - vars.pre) / vars.pre
         norm.by <- exps.pre[delta.exps] / exps.diff[delta.exps]
         p.effects <- vars.diff * norm.by
-        
+
+        if (outliers.rm) {
+            # Remove outliers, based on the "median/MAD method" presented in
+            # AMC Technical Brief number 6, April 2001 (ISSN 1757-5958).
+            # list of [var.name, row]
+
+            # Provide a median function that removes NA values.
+            fn.median <- function(x) { return(median(x, na.rm=TRUE)) }
+
+            # Calculate the median of each elementary effect.
+            p.medians <- lapply(p.effects, fn.median)
+            # Calculate the deviations from the median.
+            p.devns <- p.effects - p.medians
+            # Calculate the median of these deviations.
+            p.med.devn <- lapply(abs(p.devns), fn.median)
+            # Normalise the deviations by the median of the deviations.
+            p.med.devn[p.med.devn == 0] <- NA
+            p.devn.magn <- abs(p.devns) / p.med.devn
+
+            # Consider deviations more than 2.5 times the median deviation to
+            # be outliers, and remove them from the elementary effects table.
+            p.outlier.count <- sum(p.devn.magn > 2.5, na.rm=TRUE)
+            p.all.count <- prod(dim(p.effects))
+            p.outlier.pcnt <- round(100 * p.outlier.count / p.all.count)
+            print(paste("  Outliers: ", p.outlier.pcnt, "%", sep=""),
+                  quote=FALSE)
+            p.effects[p.devn.magn > 2.5] <- NA
+        }
+
+        # Use the built-in mean() and sd() functions, removing NA values.
         effects[[p]] <- list(
-            mean=as.list(mean(p.effects, na.rm=na.rm)),
-            sd=as.list(sd(p.effects, na.rm=na.rm)))
-        
+            mean=as.list(mean(p.effects, na.rm=TRUE)),
+            sd=as.list(sd(p.effects, na.rm=TRUE)))
+
         if (! stats.only) {
             effects[[p]]$effects <- p.effects
         }
@@ -81,11 +114,13 @@ EffectsOnVariable <- function(effects, var.name) {
 #   var.name: the name of the variable.
 #   min.mean: the minimum (absolute) mean of the plotted effects.
 #   min.sd:   the minimum standard deviation of the plotted effects.
+#   stagger:  whether to stagger the parameter names on the y-axis.
 #
 # Returns:
 #   The plot object.
 #
-PlotEffectsOnVariable <- function(effects, var.name, min.mean=NA, min.sd=NA) {
+PlotEffectsOnVariable <- function(effects, var.name, min.mean=NA, min.sd=NA,
+    stagger=FALSE) {
     effects.on.v <- EffectsOnVariable(effects, var.name)
     
     ys <- as.double(effects.on.v$mean)
@@ -108,10 +143,13 @@ PlotEffectsOnVariable <- function(effects, var.name, min.mean=NA, min.sd=NA) {
         ixs <- which(abs(ys) > min.mean)
     } else if (! is.na(min.sd)) {
         ixs <- which(abs(sds) > min.sd)
-    } else {
+    } else if (length(ys) > 0) {
         ixs <- 1:length(ys)
+    } else {
+        len.msg <- paste("WARNING: no VALID values for", var.name)
+        return(NULL)
     }
-    
+
     # Remove infinite and non-numeric values.
     old.length <- length(ixs)
     invalid.means <- which(is.na(ys[ixs]) | is.infinite(ys[ixs]))
@@ -123,7 +161,7 @@ PlotEffectsOnVariable <- function(effects, var.name, min.mean=NA, min.sd=NA) {
         ixs <- ixs[- invalid.sds]
     }
     new.length <- length(ixs)
-    
+
     if (new.length == 0) {
         len.msg <- paste("WARNING: removed ALL values for", var.name)
         print(len.msg, quote=FALSE)
@@ -133,9 +171,16 @@ PlotEffectsOnVariable <- function(effects, var.name, min.mean=NA, min.sd=NA) {
                          "values out of", old.length, "for", var.name)
         print(len.msg, quote=FALSE)
     }
-    
+
+    if (stagger) {
+        x.lbls <- StaggerLabels(xs[ixs], levels=2, do.sort=FALSE,
+                                reqd="", add="                ")
+    } else {
+        x.lbls <- xs[ixs]
+    }
+
     plot.frame <- data.frame(
-        x = xs[ixs],
+        x = x.lbls,
         y = ys[ixs],
         ymin = ymins[ixs],
         ymax = ymaxs[ixs])
